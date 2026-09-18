d0c5a480386df3c6c3ebcc86f209c2c11926c8e1
Ndoumbe <client@MacBook-Air-de-Ndoumbe.local>
2026-09-18 12:17:14 +0200

    Ajouter toutes les fichiers

--- MERN-PROJECT/models/user.model.js
+++ MERN-PROJECT/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   nom: {
@@ -23,6 +24,51 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  bio: {
+    type: String,
+    default: "",
+  },
+  photo: {
+    type: String,
+    default: "",
+  },  
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+},
+
+{
+ timestamps: true,
+}
+);
+
+//play function to hash password before saving user
+userSchema.pre("save", async function () {
+  if (!this.isModified("motdepasse")) return;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.motdepasse = await bcrypt.hash(this.motdepasse, salt);
+  } catch (error) {
+    console.error("Erreur lors du hachage du mot de passe :", error);
+    throw error;
+  }
 });
 
-module.exports = mongoose.model("User", userSchema);
+const UserModel = mongoose.model("User", userSchema);
+module.exports = UserModel ;
--- MERN-PROJECT/routes/use.routes.js
+++ MERN-PROJECT/routes/use.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const authcontroller = require("../controllers/auth.controllers");
+const usercontroller = require("../controllers/user.controllers");
 
 router.post("/register", authcontroller.signup);
 
